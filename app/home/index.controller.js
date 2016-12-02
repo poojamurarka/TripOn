bc2f077a05f2ebfbcc9ae193def702515cb1ff17
Surya Kumar Kathiravan <Surya Kumar Kathiravan>
2016-12-02 13:24:53 +0100

    ui for dashboard

--- app/home/index.controller.js
+++ app/home/index.controller.js
@@ -7,14 +7,26 @@
     function Controller(UserService,ChatService,$scope) {
         var vm = this;
         vm.user = null;
+        vm.takePlaceName = takePlaceName;
+        var socket =  io();
         initController();
-
         function initController() {
             // get current user
             UserService.GetCurrent().then(function (user) {
                 vm.user = user;
             });
         }
+
+        function takePlaceName(){
+            console.log($('#placename').val());
+            socket.emit("Get Data",$('#placename').val());
+        }
+
+        socket.on('Send Database Information', function(data) {
+            console.log(data);
+        });
+
+
 
     }
 
