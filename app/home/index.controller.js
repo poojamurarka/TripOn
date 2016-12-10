67989eee55c4c72974341599c1ed0a9622dd93c8
poojamurarka <pooja.k.murarka@student.fh-kiel.de>
2016-12-10 15:39:36 -0800

    displaying dashboard result

--- app/home/index.controller.js
+++ app/home/index.controller.js
@@ -7,43 +7,66 @@
     function Controller(UserService,HotelService,EventsService,RestaurantsService,$scope) {
         var vm = this;
         vm.user = null;
+        vm.getData = getData;
         vm.listOfHotels = [];
         vm.listOfRestaurants = [];
         vm.listOfEvents = [];
         var socket =  io();
         initController();
+        $('#noRes').css("visibility", "hidden");
         function initController() {
             // get current user
             UserService.GetCurrent().then(function (user) {
                 vm.user = user;
             });
             var address = "24114";
+            if(address!="" && address != undefined) {
+                getAllData(address);
+            }
+        }
+
+        function getData(){
+            var address = $('#placename').val();
+            if(address!="" && address != undefined) {
+                getAllData(address);
+            }
+        }
+
+        function getAllData(address){
+            $('#noRes').css("visibility", "visible");
             HotelService.GetHotels(address).then(function (data) {
-                vm.listOfHotels = data;
+                if(data && data.length > 0){
+                    $('#noRes').css("visibility", "hidden");
+                    vm.listOfHotels = data;
+                    $('.Hotels').css("visibility", "visible");
+                }else{
+                    vm.listOfHotels = [];
+                    $('.Hotels').css("visibility", "hidden");
+                }
                 console.log(vm.listOfHotels);
             });
             RestaurantsService.GetRestaurants(address).then(function (data) {
-                vm.listOfRestaurants = data;
                 console.log(vm.listOfRestaurants);
+                if(data && data.length > 0){
+                    $('#noRes').css("visibility", "hidden");
+                    vm.listOfRestaurants = data;
+                    $('.restaurants').css("visibility", "visible");
+                }else{
+                    vm.listOfRestaurants = [];
+                    $('.restaurants').css("visibility", "hidden");
+                }
             });
             EventsService.GetEvents(address).then(function (data) {
-                vm.listOfEvents = data;
-                console.log(vm.listOfEvents);
+                if(data && data.length > 0){
+                    $('#noRes').css("visibility", "hidden");
+                    vm.listOfEvents = data;
+                    $('.events').css("visibility", "visible");
+                }else{
+                    vm.listOfEvents = [];
+                    $('.events').css("visibility", "hidden");
+                }
             });
-
         }
-
-       /* function takePlaceName(){
-            //console.log($('#placename').val());
-            //socket.emit("Get Data",$('#placename').val());
-			socket.emit("Get Data",{});
-        }
-
-        socket.on('Send Database Information', function(data) {
-            console.log(data);
-        });*/
-
-
 
     }
 
