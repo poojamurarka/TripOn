47d8b0fc62d12e836042b4aa0d9c92b7e394c3d3
Namitha_Gowda <namithabgowda@gmail.com>
2016-12-13 15:34:01 +0100

    Register/signup page js

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,6 +3,8 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
     var $this = $(this),
         label = $this.prev('label');
 
+
+
     if (e.type === 'keyup') {
         if ($this.val() === '') {
             label.removeClass('active highlight');
