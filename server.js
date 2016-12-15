33920c5724df1390cabd402d990ba68b92c8d7fb
jyotiben <jyotisapariya1995@gmail.com>
2016-12-15 11:13:25 +0100

    changes in view pages

--- server.js
+++ server.js
@@ -95,7 +95,6 @@ app.use('/app', require('./controllers/app.controller'));
 app.use('/api/users', require('./controllers/api/users.controller'));
 app.use('/forgetPassword', require('./controllers/forgetPassword.controller'));
 app.use('/home', require('./controllers/home.controller'));
-app.use('/feedback', require('./controllers/feedback.controller'));
 app.use('/contact', require('./controllers/contact.controller'));
 app.use('/event_view', require('./controllers/event_view.controller'));
 app.use('/event_view1', require('./controllers/event_view1.controller'));
